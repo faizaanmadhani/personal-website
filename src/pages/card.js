3679e904a061c25a7679ef8257d40a90a1c892b6
Faizaan Madhani <fzmadhani@gmail.com>
2020-06-20 12:02:04 -0400

    Father's day card

---
+++ src/pages/card.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import View from 'react'
+import {Box, Container, Grid} from '@material-ui/core'
+import image from './faizaan_dad.png'
+import cardStyles from './card.module.css'
+
+console.log(image)
+
+function HappyCard(props) {
+    return ( 
+        <Container>
+                    <Container style={{textAlign: "center"}}>
+                        <div text-align="center"><h1>Happy Father's Day!</h1></div>
+                        <img src={image}></img>
+                    </Container>
+                    <Container>
+                        <div text-align="left">
+                            <p className={cardStyles.card}>
+                                Hi Dad, <br></br><br></br>
+                                Thanks for supporting me in everything I do, in all the goals I've tried to accomplish and in all the ambitions I have.<br></br><br></br>
+                                I wouldn't be where I am without you. <br></br>
+                                Without the sacrifices you have made. <br></br>
+                                Without everything you've done. <br></br>
+                            </p>
+                            <p className={cardStyles.card}>
+                                I love that I can have conversations with you about interesting, controversial topics.<br></br>
+                                That you have an open mind. <br></br>
+                                That you're a treasure trove of wisdom. <br></br><br></br>
+                                We'll see you soon.
+                            </p>
+                            <p className={cardStyles.card}>
+                                Faizaan
+                            </p>
+                            
+                            </div>
+                    </Container>
+        </Container>
+    )
+}
+
+export default HappyCard
