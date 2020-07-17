3232f98c03daf5d3a39a5af83dc2b9015f26bc08
Faizaan Madhani <fzmadhani@gmail.com>
2020-07-16 21:05:17 -0400

    Reby=uild with React Native

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import Profile from './components/announce/profile'
+
+function App() {
+  return (
+    <Profile />
+  );
+}
+
+export default App;
