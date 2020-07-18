9690048b89250044cbffa358e706bcc36591479a
Faizaan Madhani <fzmadhani@gmail.com>
2020-07-17 21:17:02 -0400

    Some flex

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import Profile from './components/announce/profile'
+import Profile from './components/announce/profile/profile'
 
 function App() {
   return (
---
+++ src/components/announce/profile/profile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import { Container, Row, Col } from 'reactstrap';
+import styled from 'styled-components'
+import "./profile.css"
+
+
+const Profile = (props) => {
+    return (
+      <div className="container">
+        <div>
+          Hi, my name is Faizaan. This is a profile summary. 
+        </div>
+      </div>
+    )
+}
+
+export default Profile
