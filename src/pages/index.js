38e728b4d0d8e76948a00c64e961502fe7b8068c
Faizaan Madhani <fzmadhani@gmail.com>
2020-07-31 10:48:15 -0400

    Add resume

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ const BlogIndex = ({ data, location }) => {
         Hi!
       </h3>
       <p>
-      For recruiters, here's my <a href="https://drive.google.com/file/d/11j1k_YYtvO2iFDv8VyAY7vFLGxJev1yU/view?usp=sharing">resume!</a> I'm a 2A Computer Science student at the University of Waterloo. In the past, I've interned at Human_Code and HiRide, and I'm currently seeking internships for Spring/Winter 2021. <br></br><br></br>
+      For recruiters, here's my <a href="https://drive.google.com/file/d/14w59RUejrrd9CUXb3Zr1g79z5qWy-Vtf/view?usp=sharing">resume!</a> I'm a 2A Computer Science student at the University of Waterloo. In the past, I've interned at Human_Code and HiRide, and I'm currently seeking internships for Spring/Winter 2021. <br></br><br></br>
 
 I'm passionate about Web and Mobile Development, and Software Infastructure. I'm ardent about using tech for social good and volunteer as a Project Developer for UW Blueprint, where the applications I've worked on have helped Not-for-Profit organizations be successful. <br></br><br></br>
 
